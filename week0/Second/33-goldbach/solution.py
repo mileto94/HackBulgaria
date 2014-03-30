d34a1e55ceea0d8422a8f5c96621a9d95f89a427
milka <mileto94@abv.bg>
2014-03-30 15:07:22 +0300

    Repair mistakes in code

--- week0/Second/33-goldbach/solution.py
+++ week0/Second/33-goldbach/solution.py
@@ -1,27 +1,22 @@
 def is_prime(n):
-    n = abs (n)
+    n = abs(n)
     count = 1
     if n == 1:
         return False
-    for i in range (2,n):
+    for i in range(2, n + 1):
         if n % i == 0:
             count += 1
     if count > 2:
         return False
-    else:
-        return True
+    return True
+
 
 def goldbach(n):
     result = []
-    arr = [x for x in range(2,n)]
-    for item in arr:
-        if not is_prime(item):
-            arr.remove(item)
-
+    arr = list(x for x in range(2, n) if is_prime(x))
     for i in range(len(arr)):
-        for j in range(i+1, len(arr)):
+        for j in range(i, len(arr)):
             if arr[i] + arr[j] == n:
                 temp_tuple = (arr[i], arr[j])
                 result.append(temp_tuple)
-
     return result
--- week0/Second/33-goldbach/tests.py
+++ week0/Second/33-goldbach/tests.py
@@ -1,19 +1,20 @@
 import solution
 import unittest
 
+
 class TestGoldbach(unittest.TestCase):
     """docstring for TestGoldbach"""
     def test_1(self):
-        self.assertEqual([(2,2)], solution.goldbach(4))
+        self.assertEqual([(2, 2)], solution.goldbach(4))
 
     def test_2(self):
-        self.assertEqual([(3,3)], solution.goldbach(6))
+        self.assertEqual([(3, 3)], solution.goldbach(6))
 
     def test_3(self):
-        self.assertEqual([(3,5)], solution.goldbach(8))
+        self.assertEqual([(3, 5)], solution.goldbach(8))
 
     def test_4(self):
-        self.assertEqual([(3,7), (5,5)], solution.goldbach(10))
+        self.assertEqual([(3, 7), (5, 5)], solution.goldbach(10))
 
     def test_5(self):
         self.assertEqual([(3, 97), (11, 89), (17, 83), (29, 71), (41, 59), (47, 53)], solution.goldbach(100))
